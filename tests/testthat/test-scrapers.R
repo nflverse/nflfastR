29e02d475e635ba45494372c131cf818e8c653c0
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-09-04 19:32:44 -0400

    get rid of pp

--- tests/testthat/test-scrapers.R
+++ tests/testthat/test-scrapers.R
@@ -21,29 +21,3 @@ test_that("Scraper with source live works for an old and new game at once: no pp
   expect_identical(x4[2], desc_2_live_source)
 })
 
-
-test_that("Scraper with source nfl works for an old and new game at once: pp", {
-
-  skip_if_not_installed('furrr')
-  skip_if_not_installed('future')
-
-  x1 <- fast_scraper(game_ids, pp = T) %>%
-    extract_desc()
-
-  expect_identical(x1[1], desc_1_nfl_source)
-  expect_identical(x1[2], desc_2_nfl_source)
-})
-
-
-test_that("Scraper with source live works for an old and new game at once: pp", {
-
-  skip_if_not_installed('furrr')
-  skip_if_not_installed('future')
-
-  x3 <- fast_scraper(old_game_ids, source = "live", pp = T) %>%
-    extract_desc()
-
-  expect_identical(x3[1], desc_1_nfl_source)
-  expect_identical(x3[2], desc_2_live_source)
-})
-
