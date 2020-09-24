b20de976d1c6a111b38f6f954323c430ec62ed29
mrcaseb <roemerle@gmail.com>
2020-09-24 14:35:07 +0200

    Use purrr for small samples and now really decode all player id columns

--- R/helper_decode_player_ids.R
+++ R/helper_decode_player_ids.R
@@ -11,6 +11,8 @@
 #' \code{passer_id}, \code{rusher_id}, \code{receiver_id}, \code{id} of an
 #' nflfastR play-by-play Data set and decode the player IDs to the commonly
 #' known GSIS ID format 00-00xxxxx.
+#' The function requires the package \code{furrr} if the data frame
+#' \code{pbp} has more than 4500 rows.
 #' @return The input Data Frame of the parameter 'pbp' with decoded player IDs.
 #' @importFrom rlang .data
 #' @importFrom dplyr mutate_at vars mutate pull
@@ -19,25 +21,36 @@
 #' @importFrom stringr str_sub str_replace_all str_length
 #' @export
 decode_player_ids <- function(pbp) {
-  if (!requireNamespace("furrr", quietly = TRUE)) {
-    stop("Package \"furrr\" needed to run this function. Please install/load it.")
+  if (!requireNamespace("furrr", quietly = TRUE) & nrow(pbp) > 4500) {
+    stop("Package \"furrr\" required to decode big data frames. Please install/load it.")
+  } else if (requireNamespace("furrr", quietly = TRUE) & nrow(pbp) > 4500) {
+    message("Start decoding, please wait...")
+    future::plan("multiprocess")
+    pp <- TRUE
+  } else {
+    message("Start decoding, please wait...")
+    pp <- FALSE
   }
-  message("Start decoding, please wait...")
-  future::plan("multiprocess")
+
   ret <- pbp %>%
     dplyr::mutate_at(
-      dplyr::vars(tidyselect::any_of(c(
-        "passer_id", "rusher_id", "receiver_id", "id",
+      dplyr::vars(
+        tidyselect::any_of(c("passer_id", "rusher_id", "receiver_id", "id")),
         tidyselect::ends_with("player_id")
-      ))),
-      decode_ids
+      ),
+      decode_ids, pp
     )
   message("Decoding completed.")
   return(ret)
 }
 
-decode_ids <- function(var) {
-  return(furrr::future_map_chr(var, convert_to_gsis_id))
+decode_ids <- function(var, parproc = FALSE) {
+  if (parproc == TRUE) {
+    ret <- furrr::future_map_chr(var, convert_to_gsis_id)
+  } else {
+    ret <- purrr::map_chr(var, convert_to_gsis_id)
+  }
+  return(ret)
 }
 
 convert_to_gsis_id <- function(new_id) {
