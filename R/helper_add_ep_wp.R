a1872dd35df6a05f4f9d58c24b40b9bac61eefd2
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-09-26 10:03:57 -0400

    more PAT fixes

--- R/helper_add_ep_wp.R
+++ R/helper_add_ep_wp.R
@@ -338,23 +338,30 @@ add_ep_variables <- function(pbp_data) {
   base_ep_preds$TwoPoint_Prob <- 0
 
   # Find the indices for these types of plays:
-  extrapoint_i <- which(pbp_data$play_type == "extra_point" & pbp_data$play_type_nfl != "PAT2")
+  extrapoint_i <- which((pbp_data$play_type == "extra_point" | pbp_data$play_type_nfl == "XP_KICK") & pbp_data$play_type_nfl != "PAT2")
   twopoint_i <- which(pbp_data$two_point_attempt == 1)
 
   #new: special case for PAT or kickoff with penalty
   #for inserting NAs
-  st_penalty_i <- which(
-    # pat
-    (dplyr::lag(pbp_data$touchdown == 1) & (dplyr::lead(pbp_data$two_point_attempt)==1 | dplyr::lead(pbp_data$extra_point_attempt)==1)) |
-      #kickoff
-      ((dplyr::lag(pbp_data$two_point_attempt)==1 | dplyr::lag(pbp_data$extra_point_attempt)==1) & dplyr::lead(pbp_data$kickoff_attempt == 1)) |
-      # kick formation + NA down
-      (stringr::str_detect(pbp_data$desc, 'Kick formation') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
-      (stringr::str_detect(pbp_data$desc, 'Pass formation') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
-      (stringr::str_detect(pbp_data$desc, 'kicks onside') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
-      (stringr::str_detect(pbp_data$desc, 'Offside on Free Kick') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
-      (stringr::str_detect(pbp_data$desc, 'TWO-POINT CONVERSION') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play')
+  st_penalty_i_1 <- which(
+    # pat: prior play was TD or PAT and next play is PAT
+    ((dplyr::lag(pbp_data$touchdown == 1) | dplyr::lag(pbp_data$play_type_nfl == "XP_KICK")) &
+          (dplyr::lead(pbp_data$two_point_attempt)==1 | dplyr::lead(pbp_data$extra_point_attempt)==1 | dplyr::lead(pbp_data$play_type_nfl) == "XP_KICK")) |
+      #kickoff: prior play was PAT and next play is kickoff
+      ((dplyr::lag(pbp_data$two_point_attempt)==1 | dplyr::lag(pbp_data$extra_point_attempt)==1) & dplyr::lead(pbp_data$kickoff_attempt == 1))
     )
+
+  st_penalty_i_2 <- which(
+      is.na(dplyr::lead(.data$down)) &
+         # has a key term in desc
+         (((stringr::str_detect(pbp_data$desc, 'Kick formation') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
+             (stringr::str_detect(pbp_data$desc, 'Pass formation') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
+             (stringr::str_detect(pbp_data$desc, 'kicks onside') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
+             (stringr::str_detect(pbp_data$desc, 'Offside on Free Kick') & is.na(pbp_data$down) & pbp_data$play_type == 'no_play') |
+             (stringr::str_detect(pbp_data$desc, 'TWO-POINT CONVERSION')) &
+             # down is NA and play type no play and next play isn't a kickoff
+             is.na(pbp_data$down) & pbp_data$play_type == 'no_play' & dplyr::lead(pbp_data$kickoff_attempt) == 0))
+  )
 
   # Assign the make_fg_probs of the extra-point PATs:
   base_ep_preds$ExPoint_Prob[extrapoint_i] <- make_fg_prob[extrapoint_i]
@@ -413,7 +420,8 @@ add_ep_variables <- function(pbp_data) {
                                  (1 * .data$ExPoint_Prob) + (2 * .data$TwoPoint_Prob))
 
   #just going to set these to NA bc we have no way of calculating EPA for them
-  pbp_data_ep$ExpPts[st_penalty_i] <- NA_real_
+  pbp_data_ep$ExpPts[st_penalty_i_1] <- NA_real_
+  pbp_data_ep$ExpPts[st_penalty_i_2] <- NA_real_
 
   #################################################################
   # Calculate EPA:
@@ -834,15 +842,15 @@ add_wp_variables <- function(pbp_data) {
       wp =
         dplyr::if_else((.data$kickoff_attempt == 0 & !(stringr::str_detect(.data$desc, 'Onside Kick')) &
                           (stringr::str_detect(.data$desc, 'Kick formation') | stringr::str_detect(.data$desc, 'Pass formation')) & is.na(.data$down)) |
-                            stringr::str_detect(.data$desc, 'extra point') |
+                            stringr::str_detect(.data$desc, 'TWO-POINT CONVERSION ATTEMPT') | stringr::str_detect(.data$desc, 'extra point') |
                             !is.na(.data$two_point_conv_result) |
                             !is.na(.data$extra_point_result),
                           1 - .data$wp, .data$wp),
       vegas_wp =
         dplyr::if_else((.data$kickoff_attempt == 0 & !(stringr::str_detect(.data$desc, 'Onside Kick')) &
                           (stringr::str_detect(.data$desc, 'Kick formation') | stringr::str_detect(.data$desc, 'Pass formation')) & is.na(.data$down)) |
-                            stringr::str_detect(.data$desc, 'extra point') |
-                            !is.na(.data$two_point_conv_result) |
+                         stringr::str_detect(.data$desc, 'TWO-POINT CONVERSION ATTEMPT') | stringr::str_detect(.data$desc, 'extra point') |
+                         !is.na(.data$two_point_conv_result) |
                             !is.na(.data$extra_point_result),
                           1 - .data$vegas_wp, .data$vegas_wp),
       wp = dplyr::if_else(is.na(.data$posteam), NA_real_, .data$wp),
