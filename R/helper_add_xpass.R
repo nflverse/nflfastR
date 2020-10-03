037d40fb68b6c2e2ef7b54699aa2d05c29f35fc4
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-10-03 11:16:33 -0400

    Add xpass

---
+++ R/helper_add_xpass.R
@@ -0,0 +1,103 @@
+################################################################################
+# Author: Ben Baldwin
+# Stlyeguide: styler::tidyverse_style()
+################################################################################
+
+#' Add expected pass columns
+#'
+#' @param pbp is a Data frame of play-by-play data scraped using \code{\link{fast_scraper}}.
+#' @details Build columns from the expected dropback model will return NA on data prior to 2006
+#' since that was before NFL started marking scrambles. Must be run on a dataframe that has already had
+#' \code{clean_pbp()} run on it.
+#' @return The input Data Frame of the paramter 'pbp' with the following columns
+#' added:
+#' \describe{
+#' \item{xpass}{Probability of dropback scaled from 0 to 1. }
+#' \item{pass_oe}{Dropback percent over expected on a given play scaled from 0 to 100.}
+#' }
+#' @export
+#' @import dplyr
+#' @importFrom rlang .data
+
+add_xpass <- function(pbp) {
+
+  plays <- prepare_xpass_data(pbp)
+
+  if (!nrow(plays %>% dplyr::filter(.data$valid_play == 1)) == 0) {
+
+    xpass_model <- NULL
+    suppressWarnings(
+      # load the model from github because it is too big for the package
+      try(
+        load(url("https://github.com/guga31bb/nflfastR-data/blob/master/models/xpass_model.Rdata?raw=true")),
+        silent = TRUE
+      )
+    )
+
+    pred <- stats::predict(xpass_model, as.matrix(plays %>% dplyr::select(-"valid_play"))) %>%
+      tibble::as_tibble() %>%
+      dplyr::rename(xpass = "value") %>%
+      dplyr::bind_cols(plays) %>%
+      dplyr::select("xpass", "valid_play")
+
+    pbp <- pbp %>%
+      dplyr::bind_cols(pred) %>%
+      dplyr::mutate(
+        xpass = dplyr::if_else(
+          .data$valid_play == 1, .data$xpass, NA_real_
+        ),
+        pass_oe = dplyr::if_else(!is.na(.data$xpass), 100 * (.data$pass - .data$xpass), NA_real_),
+        pass_oe = dplyr::if_else(.data$rush == 0 & .data$pass == 0, NA_real_, .data$pass_oe)
+      ) %>%
+      dplyr::select(-"valid_play")
+
+    usethis::ui_done("added xpass and pass_oe")
+  } else {
+    pbp <- pbp %>%
+      dplyr::mutate(
+        xpass = NA_real_,
+        pass_oe = NA_real_
+      )
+    usethis::ui_info("No non-NA values for xpass calculation detected. xpass and pass_oe set to NA")
+  }
+
+  return(pbp)
+}
+
+
+prepare_xpass_data <- function(pbp) {
+
+  plays <- pbp %>%
+    dplyr::mutate(
+      valid_play = dplyr::if_else(
+        .data$season >= 2006 &
+          .data$play_type %in% c("no_play", "pass", "run") &
+          !is.na(.data$posteam) &
+          !is.na(.data$down) &
+          !is.na(.data$defteam_timeouts_remaining) &
+          !is.na(.data$posteam_timeouts_remaining) &
+          !is.na(.data$yardline_100) &
+          !is.na(.data$score_differential),
+        1, 0
+      )
+    ) %>%
+    make_model_mutations() %>%
+    select(
+      "valid_play",
+      "down",
+      "ydstogo",
+      "yardline_100",
+      "qtr",
+      "wp",
+      "vegas_wp",
+      "era2", "era3", "era4",
+      "score_differential",
+      "home",
+      "half_seconds_remaining",
+      "posteam_timeouts_remaining",
+      "defteam_timeouts_remaining",
+      "outdoors", "retractable", "dome"
+    )
+
+  return(plays)
+}
