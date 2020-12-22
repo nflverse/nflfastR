38c9363c761f4f5b174b04edb4735345c8a75c2f
mrcaseb <roemerle@gmail.com>
2020-12-22 12:47:55 +0100

    Add old script for wordmarks (I forgot to do that earlier)

--- data-raw/wordmarks.R
+++ data-raw/wordmarks.R
@@ -3,21 +3,28 @@ library(dplyr)
 teams <- nflfastR::teams_colors_logos %>%
   dplyr::filter(!team_abbr %in% c("LAR", "OAK", "SD", "STL"))
 
-purrr::walk(teams$team_abbr, function(x){
-
-  image <- magick::image_trim(magick::image_read(glue::glue(
+purrr::walk(teams$team_abbr, function(x) {
+  load <- glue::glue(
     "https://static.www.nfl.com/league/apps/clubs/wordmarks/{x}_fullcolor.png"
-    )))
+  ) %>%
+    magick::image_read() %>%
+    magick::image_trim()
+
+  info <- magick::image_info(load)
+
+  rl <- (700 - info$width) / 2
+  tb <- (192 - info$height) / 2
+
+  image <- magick::image_border(load, "white", glue::glue("{rl}x{tb}"))
 
   magick::image_write(image, path = glue::glue("wordmarks/{x}.png"), format = "png")
 
-  if(x == "LA") {
+  if (x == "LA") {
     magick::image_write(image, path = "wordmarks/LAR.png", format = "png")
     magick::image_write(image, path = "wordmarks/STL.png", format = "png")
-  } else if(x == "LAC"){
+  } else if (x == "LAC") {
     magick::image_write(image, path = "wordmarks/SD.png", format = "png")
-  } else if(x == "LV"){
+  } else if (x == "LV") {
     magick::image_write(image, path = "wordmarks/OAK.png", format = "png")
   }
-
 })
