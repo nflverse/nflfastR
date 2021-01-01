4b19443152dc66a80f2a92d7542f0cb8dd1adc5b
Tan Ho <38083823+tanho63@users.noreply.github.com>
2021-01-01 07:53:26 -0500

    add field names vignette and dataframe object, and pkgdown heading (#160)
    * add field names vignette and dataframe object, and pkgdown heading
    
    * add news entry
    
    * use kable instead of just straight printing
    
    * add data fields from xyac and clean_pbp, update Rbuildignore to stop warnings, add data documentation
    
    * update to not-be-a-vignette to save 400kb of installed package space
    
    * update README to avoid reference to the vignette() function
    
    * add reference to field descriptions article in README
    
    * Update DESCRIPTION
    
    * move data to vignettes section to avoid installed-package-size issues
    
    * remove data-docs

---
+++ data-raw/create_field_descriptions.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(usethis)
+
+x <- readLines("data-raw/variable_list.txt")
+
+field_descriptions <- tibble(x = x) %>%
+  separate(x,"{",into = c(NA,"Field","Description")) %>%
+  mutate_all(str_remove_all,"\\}")
+
+usethis::use_data(field_descriptions,overwrite = TRUE)
