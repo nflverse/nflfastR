c40a83a34551f32e3f9943db2c185fb11f9ba0a1
mrcaseb <roemerle@gmail.com>
2021-01-31 20:12:11 +0100

    Change defaults in build_nflfastR_pbp deprecate arguments, include `add_xpass()`

--- R/build_nflfastR_pbp.R
+++ R/build_nflfastR_pbp.R
@@ -10,13 +10,14 @@
 #' Build a Complete nflfastR Data Set
 #'
 #' @description
-#' `build_nflfastR_pbp` is a convenient wrapper around 5 nflfastR functions:
+#' `build_nflfastR_pbp` is a convenient wrapper around 6 nflfastR functions:
 #'
 #' \itemize{
 #'  \item{[fast_scraper()]}
 #'  \item{[clean_pbp()]}
 #'  \item{[add_qb_epa()]}
 #'  \item{[add_xyac()]}
+#'  \item{[add_xpass()]}
 #'  \item{[decode_player_ids()]}
 #' }
 #'
@@ -27,14 +28,6 @@
 #' @param rules If `FALSE`, printing of the header and footer in the console output will be suppressed.
 #' @return An nflfastR play-by-play data frame like it can be loaded from <https://github.com/guga31bb/nflfastR-data>.
 #' @details To load valid game_ids please use the package function [fast_scraper_schedules()].
-#'
-#' The `source` parameter controls from which source the data is being
-#' loaded. The old parameters `rs` as well as `gc`
-#' are not valid anymore. Please use `nfl` or `old`.
-#'
-#' The `pp` parameter controls if the scraper should use parallel processing.
-#' Please note that the initiating process takes a few seconds which means it
-#' may be better to set `pp = FALSE` if you are scraping just a few games.
 #' @importFrom rlang .data
 #' @importFrom dplyr mutate_at vars mutate pull
 #' @importFrom tidyselect any_of ends_with
@@ -46,8 +39,35 @@
 #' # Build nflfastR pbp for the 2018 and 2019 Super Bowls
 #' build_nflfastR_pbp(c("2018_21_NE_LA", "2019_21_SF_KC"))
 #' }
-build_nflfastR_pbp <- function(game_ids, source = "nfl", pp = FALSE, ..., decode = FALSE, rules = TRUE) {
-  if (rules) rule_header("Build nflfastR Play-by-Play Data")
+build_nflfastR_pbp <- function(game_ids,
+                               source = lifecycle::deprecated(),
+                               pp = lifecycle::deprecated(),
+                               ...,
+                               decode = TRUE,
+                               rules = TRUE) {
+
+  if (lifecycle::is_present(source)) {
+    lifecycle::deprecate_warn(
+      when = "4.0.0",
+      what = "build_nflfastR_pbp(source = )",
+      details = "The source argument isn't used anymore and will be dropped in a future release."
+    )
+  }
+
+  if (lifecycle::is_present(pp)) {
+    lifecycle::deprecate_warn(
+      when = "4.0.0",
+      what = "build_nflfastR_pbp(pp = )",
+      details = glue::glue(
+        "We have dropped the in-package activation of parallel processing as ",
+        "this is considered bad practice.\n",
+        "Please choose an appropriate plan before calling the function, e.g. ",
+        "{usethis::ui_code('future::plan(\"multisession\")')}"
+      )
+    )
+  }
+
+  if (isTRUE(rules)) rule_header("Build nflfastR Play-by-Play Data")
 
   game_count <- length(game_ids)
   builder <- TRUE
@@ -61,14 +81,14 @@ build_nflfastR_pbp <- function(game_ids, source = "nfl", pp = FALSE, ..., decode
   ret <- fast_scraper(game_ids = game_ids, source = source, pp = pp, ..., in_builder = builder) %>%
     clean_pbp(in_builder = builder) %>%
     add_qb_epa(in_builder = builder) %>%
-    add_xyac(in_builder = builder) #%>%
-    # add_xpass(in_builder = builder)
+    add_xyac(in_builder = builder) %>%
+    add_xpass(in_builder = builder)
 
-  if (decode) {
+  if (isTRUE(decode)) {
     ret <- decode_player_ids(ret, in_builder = builder)
   }
 
-  if (rules) rule_footer("DONE")
+  if (isTRUE(rules)) rule_footer("DONE")
 
   return(ret)
 }
