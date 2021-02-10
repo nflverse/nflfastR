68056d5b4aebe1bcfd124ae76a6da407cc567990
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-02-10 13:27:14 -0500

    language edits

--- R/nflfastR-package.R
+++ R/nflfastR-package.R
@@ -2,56 +2,56 @@
 #'
 #' ## Preface
 #'
-#' Prior to nflfastR v4.0 parallel processing could be activated with an
+#' Prior to nflfastR v4.0, parallel processing could be activated with an
 #' argument `pp` in the relevant functions and progress updates were always
-#' shown. Both of these methods were bad practice and therefore removed
+#' shown. Both of these methods are bad practice and were therefore removed
 #' in nflfastR v4.0
 #'
-#' The next sections will describe how to make nflfastR work in parallel processes
-#' and show progress updates only if the user really wants to.
+#' The next sections describe how to make nflfastR work in parallel processes
+#' and show progress updates if the user wants to.
 #'
 #' ## More Speed Using Parallel Processing
 #'
-#' Nearly all functions across the nflfastR package support parallel processing
+#' Nearly all nflfastR functions support parallel processing
 #' using [furrr::future_map()] if it is enabled by a call to [future::plan()]
 #' prior to the function call.
-#' Please see the documentation of the aforementioned functions for detailed
-#' information.
+#' Please see the documentation of the functions for detailed information.
 #'
-#' As an example the following code block will resolve all function calls in the
-#' current session using multiple sessions in the background:
+#' As an example, the following code block will resolve all function calls in the
+#' current session using multiple sessions in the background and load play-by-play
+#' data for the 2018 through 2020 seasons:
 #' ```
 #' future::plan("multisession")
-#' load_pbp(seasons)
+#' load_pbp(2018:2020)
 #' build_nflfastR_pbp(game_ids)
 #' ```
-#' It is recommended to chose a default parallel processing method and save it
+#' We recommended choosing a default parallel processing method and saving it
 #' as an environment variable in the R user profile to make sure all futures
 #' will be resolved with the chosen method by default.
 #' This can be done by following the below given steps.
 #'
-#' Run the following line and the user profile should be opened automatically.
+#' First, tun the following line and the user profile should be opened automatically.
 #' If you haven't saved any environment variables yet, this will be an empty file.
 #' ```
 #' usethis::edit_r_environ()
 #'```
-#' In the now opened file add the next line, save and restart your R session.
+#' In the opened file add the next line, then save the file and restart your R session.
 #' Please note that this example sets "multisession" as default. For most users
 #' this should be the appropriate plan but please make sure it truly is.
 #' ```
 #' R_FUTURE_PLAN="multisession"
 #' ```
-#' After the session is freshly started please check if the above method worked
+#' After the session is freshly retarted please check if the above method worked
 #' by running the next line. If the output is `FALSE` you successfully set up a
 #' default non-sequential [future::plan()]. If the output is `TRUE` all functions
-#' will behave like they were called with [purrr::map()].
+#' will behave like they were called with [purrr::map()] and NOT in multisession.
 #' ```
 #' inherits(future::plan(), "sequential")
 #' ```
 #'
 #' ## Get Progress Updates while Functions are Running
 #'
-#' Most functions across the nflfastR package are able to show progress updates
+#' Most nflfastR functions are able to show progress updates
 #' using [progressr::progressor()] if they are turned on before the function is
 #' called. There are at least two basic ways to do this by either activating
 #' progress updates globally (for the current session) with
@@ -60,7 +60,7 @@
 #' ```
 #' or by piping the function call into [progressr::with_progress()]:
 #' ```
-#' load_pbp(seasons) %>%
+#' load_pbp(2018:2020) %>%
 #'   progressr::with_progress()
 #' ```
 #'
