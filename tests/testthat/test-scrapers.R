c0382e9c59043137a648a3c3f4865b0dc0ecac09
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-02-10 13:54:52 -0500

    fix check notes/errors

--- tests/testthat/test-scrapers.R
+++ tests/testthat/test-scrapers.R
@@ -2,22 +2,13 @@ context("scrapers")
 
 source('helpers.R')
 
-test_that("Scraper with source nfl works for an old and new game at once: no pp", {
+test_that("Scraper works for an old and new game at once", {
 
-  x2 <- fast_scraper(game_ids, pp = F) %>%
+  x2 <- fast_scraper(game_ids) %>%
     extract_desc()
 
   expect_identical(x2[1], desc_1_nfl_source)
   expect_identical(x2[2], desc_2_nfl_source)
-})
-
-test_that("Scraper with source live works for an old and new game at once: no pp", {
-
-  x4 <- fast_scraper(old_game_ids, source = "old", pp = F) %>%
-    extract_desc()
-
-  expect_identical(x4[1], desc_1_nfl_source)
-  expect_identical(x4[2], desc_2_live_source)
 })
 
 test_that("Wrapper with source nfl works for an old and new game at once", {
@@ -44,10 +35,10 @@ test_that("Wrapper with source nfl works for an old and new game at once", {
   expect_identical(descriptions[1], desc_1_nfl_source)
   expect_identical(descriptions[2], desc_2_nfl_source)
 
-  expect_identical(names[1], "B.Roethlisberger")
+  expect_identical(names[1], "J.Anderson")
   expect_identical(names[2], "A.Jones")
 
-  expect_identical(ids[1], "32013030-2d30-3032-3239-3234f41ff7f7")
+  expect_identical(ids[1], "00-0000316")
   expect_identical(ids[2], "32013030-2d30-3033-3332-3933ed82c0de")
 
 })
@@ -63,7 +54,7 @@ test_that("Decoding player IDs works", {
     dplyr::ungroup() %>%
     dplyr::pull(id)
 
-  expect_identical(ids[1], "00-0022924")
+  expect_identical(ids[1], "00-0000316")
   expect_identical(ids[2], "00-0033293")
 
 })
