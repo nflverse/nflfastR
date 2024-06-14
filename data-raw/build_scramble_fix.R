a96171e25b97a602ceda71543e87ecad0c37ae00
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2024-06-14 08:58:24 -0400

    make it actually work

--- data-raw/build_scramble_fix.R
+++ data-raw/build_scramble_fix.R
@@ -1,10 +1,21 @@
 library(tidyverse)
 
-# yards gained needed to match some plays in older years but not available to match in 2005. set it equal to 0 for 2005
-
 pbp <- nflfastR::load_pbp(1999 : 2005) %>%
-  select(season, game_id, play_id, week, away_team, home_team, posteam, qtr, down, ydstogo, time, yards_gained) |>
-  mutate(yards_gained = ifelse(season == 2005, 0, yards_gained))
+  # plays that could plausibly be scramble
+  filter(
+    !is.na(rusher_player_id) | penalty == 1,
+    is.na(passer_player_id),
+    is.na(receiver_player_id)
+    ) |>
+  select(season, game_id, play_id, week, away_team, home_team, posteam, qtr, down, ydstogo, time, desc) |>
+  # not in scramble data this year
+  mutate(
+    time = case_when(
+      nchar(time) == 3 ~ paste0("00", time),
+      nchar(time) == 4 ~ paste0("0", time),
+      TRUE ~ time
+    )
+    )
 
 # Thank you to Aaron Schatz and Football Outsiders
 # For the charting data to fix scrambles in 2005
@@ -13,8 +24,7 @@ s <- readxl::read_xlsx("data-raw/scrambles_2005.xlsx") %>%
   janitor::clean_names() %>%
   select(
     season = year, week, qtr, away_team = away, home_team = home, posteam = offense, down, ydstogo = togo, date_time = time
-  ) |>
-  mutate(yards_gained = 0)
+  )
 
 # Thank you to Aaron Schatz
 # For the charting data to fix scrambles in 1999 - 2004
@@ -40,15 +50,32 @@ dat <- bind_rows(
   select(-date_time) %>%
   mutate_at(vars(home_team, away_team, posteam), nflfastR:::team_name_fn)
 
+# 1999: need to join on yards_gained ?
+
 d <- dat %>%
   dplyr::left_join(
     pbp,
-    by = c("week", "away_team", "home_team", "posteam", "qtr", "down", "ydstogo", "time", "season", "yards_gained")
+    by = c("week", "away_team", "home_team", "posteam", "qtr", "down", "ydstogo", "time", "season")
   ) %>%
   mutate(scramble_id = paste0(game_id, "_", play_id)) %>%
   filter(scramble_id != "2005_09_CIN_BAL_1725")
 
+
+
+# number non-matched by season
+  nrow(d)
+  d |> filter(is.na(desc)) |> group_by(season) |> summarise(n = n())
+
+# get rid of non-match
+  d <- d |>
+    filter(!is.na(desc))
+  d |> group_by(season) |> summarise(n = n())
+
 scramble_fix <- d$scramble_id
+scramble_fix <- scramble_fix |>
+  unique()
+length(scramble_fix)
 saveRDS(scramble_fix, file = "data-raw/scramble_fix.rds")
+
 
 
