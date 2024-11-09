cb83260032c33891c66fa2d33655337391ae6074
mrcaseb <mrcaseb@gmail.com>
2024-11-09 17:15:21 +0100

    document differences to old stats

--- data-raw/nfl_stats_variables.R
+++ data-raw/nfl_stats_variables.R
@@ -24,13 +24,42 @@ nfl_stats_variables <- jsonlite::fromJSON("data-raw/nfl_stats_variables.json")
 
 usethis::use_data(nfl_stats_variables, overwrite = TRUE)
 
-s_old_1 <- nflreadr::load_player_stats(2024, "offense")
-s_old_2 <- nflreadr::load_player_stats(2024, "defense")
-s_old_3 <- nflreadr::load_player_stats(2024, "kicking")
+s_old_1 <- nflreadr::load_player_stats(2023, "offense")
+s_old_2 <- nflreadr::load_player_stats(2023, "defense")
+s_old_3 <- nflreadr::load_player_stats(2023, "kicking")
 n_old_1 <- names(s_old_1)
 n_old_2 <- names(s_old_2)
 n_old_3 <- names(s_old_3)
 
-setdiff(n2, n_old_1)
-setdiff(n2, n_old_3)
+
+# Differences to old offense stats ----------------------------------------
+
+# player_name -> player_display_name
+# recent_team -> team (recent team in weekly data never made sense)
+# interceptions -> passing_interceptions (all passing stats have the passing prefix)
+# sacks -> sacks_suffered (to make clear it's not on defensive side)
+# sack_yards -> sack_yards_lost (to make clear it's not on defensive side)
+# dakota -> not implemented at the moment
 setdiff(n_old_1, n2)
+setdiff(n2, n_old_1)
+
+# Differences to old defense stats ----------------------------------------
+
+# player_name -> player_display_name
+# def_tackles -> there is def_tackles_solo and def_tackles_with_assist
+# def_fumble_recovery_own -> fumble_recovery_own (it is not exclusive to defense)
+# def_fumble_recovery_yards_own -> fumble_recovery_yards_own (it is not exclusive to defense)
+# def_fumble_recovery_opp -> fumble_recovery_opp (it is not exclusive to defense)
+# def_fumble_recovery_yards_opp -> fumble_recovery_yards_opp (it is not exclusive to defense)
+# def_safety -> def_safeties (we use plural everywhere)
+# def_penalty -> penalties (it is not exclusive to defense)
+# def_penalty_yards -> penalty_yards (it is not exclusive to defense)
+setdiff(n_old_2, n2)
+setdiff(n2, n_old_2)
+
+
+# Differences to old kicking stats ----------------------------------------
+
+# player_name -> player_display_name
+setdiff(n_old_3, n2)
+setdiff(n2, n_old_3)
